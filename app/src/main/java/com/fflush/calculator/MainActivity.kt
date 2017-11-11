a8da0ffdd1111d3536902bc4b23a85d87749e4d1
Martin <martin.e.castro.88@gmail.com>
2017-11-11 17:22:13 -0300

    Main activity: functions for input and some operations developed Layout: now fits with different screen size Colors: they were set for the buttons

--- app/src/main/java/com/fflush/calculator/MainActivity.kt
+++ app/src/main/java/com/fflush/calculator/MainActivity.kt
@@ -2,11 +2,91 @@ package com.fflush.calculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //Variables used
+    var op = "*"
+    var oldNumber = ""
+    var isNewOperation = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonNumberEvent(view: View){
+        if (isNewOperation){
+            showNumbersText.setText("")
+        }
+        isNewOperation = false
+        val buttonSelected = view as Button
+        var buttonClickedNumber: String = showNumbersText.text.toString()
+        when(buttonSelected.id){
+            buttonNumber0.id -> {
+                buttonClickedNumber += "0"
+            }
+            buttonNumber1.id -> {
+                buttonClickedNumber += "1"
+            }
+            buttonNumber2.id -> {
+                buttonClickedNumber += "2"
+            }
+            buttonNumber3.id -> {
+                buttonClickedNumber += "3"
+            }
+            buttonNumber4.id -> {
+                buttonClickedNumber += "4"
+            }
+            buttonNumber5.id -> {
+                buttonClickedNumber += "5"
+            }
+            buttonNumber6.id -> {
+                buttonClickedNumber += "6"
+            }
+            buttonNumber7.id -> {
+                buttonClickedNumber += "7"
+            }
+            buttonNumber8.id -> {
+                buttonClickedNumber += "8"
+            }
+            buttonNumber9.id -> {
+                buttonClickedNumber += "9"
+            }
+            buttonDot.id -> {
+                if(!buttonClickedNumber.contains(".")) {
+                    buttonClickedNumber += "."
+                }
+            }
+            buttonPosNeg.id -> {
+                if(!buttonClickedNumber.contains("-")) {
+                    buttonClickedNumber = "-$buttonClickedNumber"
+                }
+            }
+        }
+        showNumbersText.setText(buttonClickedNumber)
+    }
+
+    fun buttonOperationEvent (view: View){
+        val buttonSelected = view as Button
+        when(buttonSelected.id){
+            buttonProd.id -> {
+                op="*"
+            }
+            buttonDiv.id -> {
+                op="/"
+            }
+            buttonSum.id -> {
+                op="+"
+            }
+            buttonMin.id -> {
+                op="-"
+            }
+        }
+        oldNumber = showNumbersText.text.toString()
+        isNewOperation = true
     }
 }
